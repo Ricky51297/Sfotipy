1013012cefe5ed5c1c355ae76c3f1e574dd741b7
Ricardo López <ricardo_51297@outlook.com>
2015-04-18 22:38:22 -0500

    first commit

---
+++ tracks/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+from artists.models import Artist
+from albums.models import Album
+
+class Track(models.Model):
+	title = models.CharField(max_length=255)
+	order = models.PositiveIntegerField()
+	track_file = models.FileField(upload_to='tracks')
+	album = models.ForeignKey(Album)
+	artist = models.ForeignKey(Artist)
+
+	def __unicode__(self):
+		return self.title
